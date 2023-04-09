040faf85f840d7eae45b0d3d47263f536befb39e
Wojturski Maciej <w65578@student.wsiz.edu.pl>
2023-04-09 08:52:30 +0200

    Create HTML for navbar and sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,23 @@ import { AppComponent } from './app.component';
 import { TransactionListComponent } from './components/transaction-list/transaction-list.component';
 import {HttpClientModule} from "@angular/common/http";
 import {TransactionService} from "./services/transaction.service";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TransactionListComponent
+    TransactionListComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [TransactionService],
   bootstrap: [AppComponent]
